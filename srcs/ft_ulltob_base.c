20962d0b497674075209f88dae69f053d7c2d3c6
Nicolas Huynh <nico.huynh@gmail.com>
2019-01-03 05:32:41 +0100

    more stuff

---
+++ srcs/ft_ulltob_base.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ulltob_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/30 08:13:30 by nihuynh           #+#    #+#             */
+/*   Updated: 2018/12/27 15:08:18 by nihuynh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static inline int	ft_toupper(int c)
+{
+	return ((c >= 97 && c <= 122)) ? c - 32 : c;
+}
+
+int					ft_ulltob_base(unsigned long long value, int base, char *bf, int upc)
+{
+	char				*bstr;
+	int					idx;
+	int					res;
+	unsigned long long	carry;
+
+	bstr = "0123456789abcdef";
+	idx = (value < 0 && base == 10) ? 2 : 1;
+	carry =  value;
+	if (base < 2 || base > 16)
+		return (-1);
+	while (carry /= base)
+		idx++;
+	carry =  value;
+	res = idx;
+	bf[idx] = '\0';
+	bf[0] = (value < 0 && base == 10) ? '-' : bstr[0];
+	while (carry)
+	{
+		bf[--idx] = (upc) ? ft_toupper(bstr[carry % base]) : bstr[carry % base];
+		carry /= base;
+	}
+	return (res);
+}
